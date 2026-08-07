d87e2662e9c90f6782aee0e617e0cd9025551634
tocchekalkra <tocchekalkra@gmail.com>
2026-08-07 16:51:32 +0700

    Add React frontend to Visual Studio solution

--- WarehouseManager.Infrastructure/Database/Migrations/20260807080047_ImproveMaterialCatalogs.Designer.cs
+++ WarehouseManager.Infrastructure/Database/Migrations/20260807080047_ImproveMaterialCatalogs.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WarehouseManager.Infrastructure.Database;
@@ -11,9 +12,11 @@ using WarehouseManager.Infrastructure.Database;
 namespace WarehouseManager.Infrastructure.Database.Migrations
 {
     [DbContext(typeof(WarehouseDbContext))]
-    partial class WarehouseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260807080047_ImproveMaterialCatalogs")]
+    partial class ImproveMaterialCatalogs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ warehousemanager.web.vs/src/App.tsx
@@ -0,0 +1,16 @@
+export default function App() {
+    return (
+        <div
+            style={{
+                minHeight: '100vh',
+                background: '#090d12',
+                color: 'white',
+                display: 'grid',
+                placeItems: 'center',
+                fontFamily: 'Arial',
+            }}
+        >
+            <h1>WarehouseManager работает</h1>
+        </div>
+    );
+}
